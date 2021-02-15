0f8fd44ad7498f508b0b8b7831bb04cad096d7b3
Lys <lyscalefi@gmail.com>
2021-02-15 13:37:39 -0300

    Criação do UserProfileController

---
+++ test/src/main/java/com/concrete/api/test/controller/UserProfileController.java
@@ -0,0 +1,59 @@
+package com.concrete.api.test.controller;
+
+import com.concrete.api.test.model.ReturnSuccess;
+import com.concrete.api.test.model.User;
+import com.concrete.api.test.repositories.UserRepository;
+import exception.UnauthorizedToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import static java.util.concurrent.TimeUnit.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(value = "/userprofile/{id}")
+public class UserProfileController {
+
+    @Autowired
+    UserRepository userProfile;
+
+    @Autowired
+    HttpServletRequest request;
+
+    @GetMapping(produces = { "application/json"}, headers = "Content-type=application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public ReturnSuccess perfil(@PathVariable String id) throws UnauthorizedToken{
+
+        String token;
+        token = request.getHeader("Authorization");
+        if (token == null || token.isEmpty () ) {
+            throw new UnauthorizedToken("Não autorizado");
+        }
+
+        User user = new User();
+        user = userProfile.findById(UUID.fromString(id));
+        if (user == null || !user.getToken().equals(token)){
+            throw new UnauthorizedToken("Não autorizado");
+        }
+
+        OffsetDateTime now = OffsetDateTime.now();
+        long diff = ChronoUnit.MINUTES.between(user.getLastLogin(), now);
+        if (diff >= 30) {
+            throw new UnauthorizedToken("Não autorizado");
+        }
+        ReturnSuccess returnSuccess = new ReturnSuccess(user);
+
+        return returnSuccess;
+
+    }
+}
---
+++ test/src/main/java/exceptionHandler/ApiError.java
@@ -0,0 +1,35 @@
+package exceptionHandler;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+import java.util.Arrays;
+
+public class ApiError {
+
+    private HttpStatus status;
+    private String message;
+
+    public ApiError(HttpStatus status, String message) {
+        super();
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
